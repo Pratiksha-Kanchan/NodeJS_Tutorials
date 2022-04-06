f0fcd14631fb545a6e77a46caaffa757557a5eaa
Pratiksha Kanchan <84810132+Unmerited-Scientist278@users.noreply.github.com>
2022-04-06 17:34:10 +0530

    Create os_module.js
    https://nodejs.org/docs/latest-v14.x/api/os.html

---
+++ os_module.js
@@ -0,0 +1,36 @@
+const os = require('os');
+
+const server = {
+  'type': os.type(),
+  'architecture': os.arch(),
+  'uptime': os.uptime()
+}
+
+console.log(server)
+
+
+
+/*
+os.EOL
+os.arch()
+os.constants
+os.cpus()
+os.devNull
+os.endianness()
+os.freemem()
+os.getPriority([pid])
+os.homedir()
+os.hostname()
+os.loadavg()
+os.networkInterfaces()
+os.platform()
+os.release()
+os.setPriority([pid, ]priority)
+os.tmpdir()
+os.totalmem()
+os.type()
+os.uptime()
+os.userInfo([options])
+os.version()
+*/
+
