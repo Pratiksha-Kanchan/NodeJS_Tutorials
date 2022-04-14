4a66207bd9251837eb887e4f08b00d5c5f9f5826
Pratiksha Kanchan <84810132+Unmerited-Scientist278@users.noreply.github.com>
2022-04-14 11:33:57 +0530

    Create shoppingResults.js

---
+++ shoppingResults.js
@@ -0,0 +1,16 @@
+// create a program which reads from one text file and then writes to another. 
+
+const readline = require('readline');
+const fs = require('fs');
+
+const myInterface = readline.createInterface({
+  input: fs.createReadStream('shoppingList.txt')
+});
+
+const fileStream = fs.createWriteStream('shoppingResults.txt.')
+
+let transformData = (line) => {
+fileStream.write(`They were out of: ${line}\n`)
+}
+
+myInterface.on('line', transformData)
